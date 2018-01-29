bffae93afb82976b3346e890d5c18fca0286e6dd
krishnardt <krishnardt@users.noreply.github.com>
2018-01-30 00:23:33 +0530

    Code using Pandas for faster processing

--- Affinity.py
+++ Affinity.py
@@ -1,29 +1,38 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Jan 29 09:11:40 2018
+Created on Mon Jan 29 19:11:40 2018
 
 @author: Krishna mohan
 """
-import time
-start = time.time();
-import pandas as pd
-import numpy as np
+# pandas is Python library that will work over numpy.
+import pandas as pd                                                 
+#It will store data in dataframe type for faster calculation purpose.
+# It will convert the unstructural data into structured data computationally.
 
-data = pd.read_table("data.tsv", header = None)
+def getMovieNames():
+    data = pd.read_table("data.txt", header = None, delimiter = '\t', chunksize = 100000)    # storing the data in varable "data" as dataframe
+    # now the data contains 12 records as per example
+    data = pd.concat(data, ignore_index = True)
+    #Here input variable takes Avataar as input as per example
+    input = 'Avataar'                                                    
+    #filtering the names that contains Avataar as movie 
+    names = set(data[data[1] == input][0])                                    
+    #filtering the data from records with Avataar
+    data = data[data[1] != input]                                        
+    #now data will contain only 9 records
+    movies = set()                                                       
+    #create a set with variable "movie". Set will be helpful for getting unique values among 
+    # different viewers. It is helpful in performing union and intersection operations.
+    
+    for i in names:                                                     
+        #Iterating over each name
+        movies |= set(data[data[0] == i][1])                            
+        #performing union operation on 2 sets and storing output in movies variable
+    
+    
+    for i in movies:
+        print(i)
+    
 
-input = 'Avataar'
-names = data[data[1] == 'Avataar'].iloc[:,0]
-
-movies = set()
-
-for i in names:
-    movies = movies | set(data[data[0] == i].iloc[:,1])
-
-#movies = list(movies)
-
-movies.remove(input)
-for i in movies:
-    print(i)
-
-end = time.time()
-print(end-start)
+if __name__ == "__main__":
+    getMovieNames();
